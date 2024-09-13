2ee8ab8be068feb6c2e840fcde1964a395c01572
Iván Illera <ivanillera@gmail.com>
2024-09-13 20:57:25 -0300

    a

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ export const Card = ({ price, image, title, description }) => {
       <div className="absolute top-4 left-4 bg-red-600 text-white px-4 py-2 text-4xl font-bold rounded-full shadow-lg">
         {price}
       </div>
-      <img src={image} alt={title} className="w-full h-48 object-cover" />
+      <img src={image} alt={title} className="w-full h-48 object-contain" />
       <div className="p-4">
         <h2 className="text-xl font-semibold">{title}</h2>
         <p className="text-sm text-gray-700">{description}</p>
